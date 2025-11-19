40c319e2518e79f6b2203c850f96322c5943cd89
Mac Hale <machaledesign@gmail.com>
2025-11-18 19:08:09 -0600

    handles form submition

--- app/create-task/page.tsx
+++ app/create-task/page.tsx
@@ -1,16 +1,45 @@
 'use client';
 
-import { Button, TextArea, TextField } from '@radix-ui/themes';
-import React from 'react';
+import { Button, TextField } from '@radix-ui/themes';
+import SimpleMDE from 'react-simplemde-editor';
+import { useForm, Controller } from 'react-hook-form';
+import axios from 'axios';
+import 'easymde/dist/easymde.min.css';
+import { useRouter } from 'next/navigation';
+
+interface TaskForm {
+  title: string;
+  description: string;
+}
 
 const CreateTask = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<TaskForm>();
+
   return (
     <>
-      <div className="max-w-lg m-auto space-y-3">
-        <TextField.Root placeholder="Title"></TextField.Root>
-        <TextArea placeholder="Description" />
-        <Button>Create Task</Button>
-      </div>
+      <form
+        onSubmit={handleSubmit(async (data) => {
+          await axios.post('/api/tasks', data);
+          router.push('/')
+        })}
+        className="max-w-lg m-auto space-y-3"
+      >
+        <TextField.Root
+          placeholder="Title"
+          {...register('title')}
+        ></TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+        <div className="text-center">
+          <Button>Create Task</Button>
+        </div>
+      </form>
     </>
   );
 };
